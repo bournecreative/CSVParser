d3e538253675f2a6b70ccc6fcb1724dd4ea00e95
Christian <bournecreative@gmail.com>
2024-02-16 21:15:14 -0600

    create route for upload, pass req payload to fileUpload and log result

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Running testing");
+app.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => console.log("running"));
+app.post("/upload", fileUpload({ createParentPath: true }), (req, res) => {
+  const files = req.files;
+  console.log(files);
+  return res.json({ status: "logged", message: "logged" });
+});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
