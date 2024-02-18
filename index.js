fa7c725df337c5bae8c7788d19bb3e9ec1be6517
Christian <bournecreative@gmail.com>
2024-02-17 20:54:52 -0600

    add sass package, update scripts to watch sass and start nodemon to watch server, update server toset out compiled css into public folder, update index to reference static assets and move css from index.html into main.scss.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const home = require("./routes/home");
 const getCSV = require("./routes/getCSV");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/assets", express.static(path.join(__dirname, "/public")));
 app.use("/", home);
 app.use("/upload", getCSV);
 
