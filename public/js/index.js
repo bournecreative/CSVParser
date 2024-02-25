2eb9b33cf9f7c498dfd5addec76975cebb75744c
Christian <bournecreative@gmail.com>
2024-02-24 20:54:01 -0600

    organized logic for DOM manipulation into an object with methods

--- public/js/index.js
+++ public/js/index.js
@@ -1,101 +1,108 @@
-(function () {
-  window.onload = () => {
-    const form = document.getElementById("formLoader");
-    const inputLoader = document.getElementById("uploader");
-    const discardButton = document.querySelector(".discard");
-    const attachment = document.querySelector(".attachment");
-    const responseEle = document.querySelector(".upload-response");
+const csvParser = {
+  init() {
+    this.uploadForm = document.getElementById("formLoader");
+    if (!this.uploadForm) {
+      return;
+    }
+    this.uploadInput = this.uploadForm.querySelector(".upload-input");
+    this.discardButton = this.uploadForm.querySelector(".discard");
+    this.attachment = document.querySelector(".attachment");
+    this.messageWindow = document.querySelector(".upload-response");
 
-    function handleChange(e) {
+    this.discardButton.addEventListener(
+      "click",
+      this.handleDiscard.bind(this),
+      false
+    );
+
+    this.uploadInput.addEventListener(
+      "change",
+      this.handleChange.bind(this),
+      false
+    );
+    this.uploadForm.addEventListener("submit", (e) => {
       e.preventDefault();
-      e.stopPropagation();
-      if (e.target.files[0]) {
-        generateAttachment();
-      }
-    }
-
-    const handleDiscard = () => {
-      discardAttachment();
-    };
-
-    const sendFiles = async () => {
-      const myFile = inputLoader.files;
-      const formData = new FormData();
-      formData.append(myFile.item(0)?.name, myFile.item(0));
-
-      const response = await fetch("/upload", {
-        method: "POST",
-        body: formData,
-      });
-
-      const json = await response?.json();
-
-      if (json?.status === "error") {
-        generateErrorEle(json?.status, json?.message);
-      } else {
-        generateResponse(json?.message);
-      }
-    };
-
-    function generateAttachment() {
-      const container = document.createElement("div");
-      const textContainer = document.createElement("div");
-      const icon = document.createElement("span");
-      const title = document.createElement("h3");
-      const detail = document.createElement("span");
-      container.setAttribute("class", "attachment-msg");
-      icon.setAttribute("class", "icon");
-      detail.textContent = `${inputLoader.files[0].name}`;
-      title.textContent = "Upload Completed";
-      attachment.appendChild(container);
-      container.appendChild(icon);
-      container.appendChild(textContainer);
-      textContainer.appendChild(title);
-      textContainer.appendChild(detail);
-    }
-
-    function generateErrorEle(status, message) {
-      const container = document.createElement("div");
-      const title = document.createElement("span");
-      const detail = document.createElement("p");
-      container.setAttribute("class", "upload-response-error");
-      title.textContent = status;
-      detail.textContent = message;
-      responseEle.appendChild(container);
-      container.appendChild(title);
-      container.appendChild(detail);
-    }
-
-    function generateResponse(message) {
-      const container = document.createElement("div");
-      container.setAttribute("class", "upload-response-content");
-      const code = document.createElement("code");
-      responseEle.appendChild(container);
-      code.textContent = JSON.stringify(message, null, 2);
-      container.appendChild(code);
-    }
-
-    function discardAttachment() {
-      const target = document.querySelector(".attachment-msg");
-      if (target) {
-        attachment.removeChild(target);
-      }
-    }
-
-    function discardErrorResponse() {
-      const target = responseEle.firstChild;
-      if (target) {
-        responseEle.removeChild(target);
-      }
-    }
-
-    inputLoader.addEventListener("change", handleChange);
-    discardButton.addEventListener("click", handleDiscard);
-
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      discardErrorResponse();
-      sendFiles();
+      this.clearResponseMessages();
+      this.sendFiles();
     });
-  };
-})();
+  },
+  handleChange(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const file = e.target.files[0];
+    if (file) {
+      this.generateAttachment(file);
+    }
+  },
+  handleDiscard() {
+    this.discardAttachment();
+  },
+  async sendFiles() {
+    const payload = this.uploadInput.files;
+    const formData = new FormData();
+    formData.append(payload.item(0)?.name, payload.item(0));
+
+    const response = await fetch("/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const json = await response?.json();
+
+    if (json?.status === "error") {
+      this.generateErrorEle(json?.status, json?.message);
+    } else {
+      this.generateResponse(json?.message);
+    }
+  },
+  generateAttachment(payload) {
+    const container = document.createElement("div");
+    const textContainer = document.createElement("div");
+    const icon = document.createElement("span");
+    const title = document.createElement("h3");
+    const detail = document.createElement("span");
+    container.setAttribute("class", "attachment-msg");
+    icon.setAttribute("class", "icon");
+    detail.textContent = `${payload}`;
+    title.textContent = "Upload Completed";
+    this.attachment.appendChild(container);
+    container.appendChild(icon);
+    container.appendChild(textContainer);
+    textContainer.appendChild(title);
+    textContainer.appendChild(detail);
+  },
+  generateErrorEle(status, message) {
+    const container = document.createElement("div");
+    const title = document.createElement("span");
+    const detail = document.createElement("p");
+    container.setAttribute("class", "upload-response-error");
+    title.textContent = status;
+    detail.textContent = message;
+    this.messageWindow.appendChild(container);
+    container.appendChild(title);
+    container.appendChild(detail);
+  },
+  generateResponse(message) {
+    const container = document.createElement("div");
+    container.setAttribute("class", "upload-response-content");
+    const code = document.createElement("code");
+    this.messageWindow.appendChild(container);
+    code.textContent = JSON.stringify(message, null, 2);
+    container.appendChild(code);
+  },
+  discardAttachment() {
+    const attachmentChild = this.attachment.firstChild;
+    if (attachmentChild) {
+      this.attachment.removeChild(attachmentChild);
+    }
+  },
+  clearResponseMessages() {
+    const responseChild = this.messageWindow.firstChild;
+    if (responseChild) {
+      this.messageWindow.removeChild(responseChild);
+    }
+  },
+};
+window.onload = () => {
+  csvParser.init();
+};
